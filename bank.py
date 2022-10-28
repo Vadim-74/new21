4bc810c52f4625bde49f4bb69bb3cb9c477681cc
Vadim-74 <6514501@mail.ru>
2022-10-28 17:57:51 +0300

    [upd] Обновление

--- bank.py
+++ bank.py
@@ -4,3 +4,4 @@ per_cent = {'ТКБ': float(money) / 100 * 5.6, 'СКБ': float(money) / 100 * 5
             'СБЕР': float(money) / 100 * 4.0}
 
 print("Максимальная сумма, которую вы можете заработать", max(per_cent.values()), "руб.")
+print(money)
