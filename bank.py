5f2a58c5f0f4543d2d733486f61fe098e209fc93
Vadim-74 <6514501@mail.ru>
2022-10-28 18:25:13 +0300

    [padd] Обновление

--- bank.py
+++ bank.py
@@ -1,5 +1,6 @@
 money = input("Введите сумму вклада:")
 
-per_cent = {'ТКБ': float(money)/100*5.6, 'СКБ': float(money)/100*5.9, 'ВТБ': float(money)/100*4.28, 'СБЕР': float(money)/ 100*4.0}
+per_cent = {'ТКБ': float(money) / 100 * 5.6, 'СКБ': float(money) / 100 * 5.9, 'ВТБ': float(money) / 100 * 4.28,
+            'СБЕР': float(money) / 100 * 4.0}
 
-print("Максимальная сумма, которую вы можете заработать", max(per_cent. значения()), "руб.")
+print("Максимальная сумма, которую вы можете заработать", max(per_cent.values()), "руб.")
